5d67e701647a3347b454c0b96a570b107bff1d1c
vijayjag <vijayzuzu@gmail.com>
2019-08-01 16:35:13 -0500

    level 5: using passport.js - salting + cookies/sessions

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const app = express();
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+// const bcrypt = require('bcrypt'); - level 4: salting using bcrypt
+// const saltRounds = 10;
 // const encrypt = require('mongoose-encryption'); - level 2: database encrytion
 // const md5 = require('md5'); - level 3: hashing
 
@@ -15,7 +18,19 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: "Little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//initiliase passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
+// to avoid deprecated warning
+mongoose.set("useCreateIndex",true);
 
 //schema for email and password entered by user
 const userSchema = new mongoose.Schema({
@@ -23,6 +38,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 //level 2 database encryption. We encrypt using the secret as a plugin to the mongoose schema.
 
 //we want to encrpyt only the password.
@@ -31,6 +47,10 @@ const userSchema = new mongoose.Schema({
 // userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields: ['password']});
 
 const User = new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
     res.render("home");
@@ -44,42 +64,56 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 
+app.get("/secrets",function(req,res){
+    //if user is authenticated/already logged in, simply render the page. Else, redirect to login
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }
+    else{
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout",function(req,res){
+    //end user session
+    req.logout();
+    res.redirect("/");
+});
+
 //register using email id and password
 app.post("/register",function(req,res){
-    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
-        const newUser = new User({
-            email: req.body.username,
-            //level 3: using md5 to hash the password - md5(req.body.password)
-            password: hash
-        });
-        newUser.save(function(err){
-            if(!err){
-                res.render("secrets");
-            }
-            else{
-                console.log(err);
-            }
-        }); 
+    User.register({username: req.body.username},req.body.password,function(err,user){
+        if(err){
+            res.send(err);
+            res.redirect("/register");
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                //callback will be triggered only if authentication is successful.
+                res.redirect("/secrets");
+            });
+        }
+
     });
-    
 });
 
 //login using email and password
 app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({email: username},function(err,found){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    //we need to use passport to authenticate and login this user.
+    req.login(user,function(err){
         if(err){
-            console.log(err)
+            console.log(err);
         }
-        else{ 
-            if(found){
-                bcrypt.compare(password,found.password,function(err,result){
-                    if(result===true){
-                        res.render("secrets");
-                    }
-                });
-            }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                //callback will be triggered only if authentication is successful.
+                res.redirect("/secrets");
+            });
         }
     });
 });
